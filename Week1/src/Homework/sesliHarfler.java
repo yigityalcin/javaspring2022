ca90ca2260028060793c6ca4be04ae33710c3b81
yigit <yigityalciin@hotmail.com>
2022-11-10 01:13:34 +0300

    second commit

---
+++ Week1/src/Homework/sesliHarfler.java
@@ -0,0 +1,21 @@
+package Homework;
+
+public class sesliHarfler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char harf = 'I';
+
+        switch (harf){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Kalın sesli harf");
+                break;
+            default:
+                System.out.println("İnce sesli harf");
+        }
+	}
+
+}
