dbbd0a28371208426a94238d6cc4cca6a7bfb834
yigit <yigityalciin@hotmail.com>
2022-11-10 01:39:20 +0300

    first

---
+++ Week2/src/Homework/methods.java
@@ -0,0 +1,41 @@
+package Homework;
+
+public class methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sayiBulmaca();
+        sayiBulmaca();
+        sayiBulmaca();
+        sayiBulmaca();
+    }
+
+    //camelCase
+    public static void sayiBulmaca() {
+        int[] sayilar = new int[]{1, 2, 5, 7, 9, 0};
+        int aranacak = 5;
+        boolean varMi = false;
+
+        for (int sayi : sayilar) {
+            if (sayi == aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+
+        String mesaj = "";
+        if (varMi) {
+            mesaj = "Sayı mevcuttur: " + aranacak;
+            mesajVer(mesaj);
+        } else {
+            mesaj = "Sayı mevcut değildir: " + aranacak;
+            mesajVer(mesaj);
+        }
+
+    }
+
+    public static void mesajVer(String mesaj){
+        System.out.println(mesaj);
+	}
+
+}
---
+++ Week2/src/Homework/reCapDemo_Classes.java
@@ -0,0 +1,12 @@
+package Homework;
+
+public class reCapDemo_Classes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			DortIslem dortIslem = new DortIslem();
+	        int sonuc = dortIslem.Topla(3, 4);
+	        System.out.println(sonuc);
+	}
+
+}
