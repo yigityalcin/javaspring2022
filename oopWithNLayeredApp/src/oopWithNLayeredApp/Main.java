d193f5761490881aad85ce8c03a53f4dc9a8ba0a
yigit <yigityalciin@hotmail.com>
2022-12-19 00:29:16 +0300

    19.12

--- oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
@@ -1,10 +1,15 @@
 package oopWithNLayeredApp;
 
+import oopWithNLayeredApp.business.ProductManager;
+import oopWithNLayeredApp.entities.Product;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception  {
+		Product product1 = new Product(1,"Iphone11", 100000);
+		
+		ProductManager productManager = new ProductManager();
+		productManager.add(product1);
 	}
 
 }
